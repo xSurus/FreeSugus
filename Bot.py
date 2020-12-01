5e11b1f5abdb6efad83b5ee0a8d5a6f8337b3375
xSurus <43451416+xSurus@users.noreply.github.com>
2020-12-01 19:19:26 +0100

    IT WORKS
    reply command

--- Bot.py
+++ Bot.py
@@ -1,16 +1,26 @@
 # bot.py
 import os
-
+from discord.ext import commands
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
-
+client = commands.Bot(command_prefix="^^")
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    await client.process_commands(message)
+
+@client.command()
+async def test(ctx):
+    await ctx.send("reply")
+
 
 client.run(TOKEN)
